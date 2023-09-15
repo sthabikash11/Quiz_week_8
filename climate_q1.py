d7b2ca0ac4f8de0da838012210dfad266d5a235e
Suzan Shrestha <sthabikash11@gmail.com>
2023-09-15 16:43:23 +1000

    connected to database and fetch the values from it

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
+import sqlite3
+
+connection = sqlite3.connect('climate.db')
+cursor = connection.cursor()
+cursor.execute('SELECT Year, CO2, Temperature FROM ClimateData')
+data = cursor.fetchall()
         
-years = []
-co2 = []
-temp = []
+years = [row[0] for row in data]
+co2 = [row[1] for row in data]
+temp = [row[2] for row in data]
+
+connection.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -16,3 +24,4 @@ plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)") 
 plt.show() 
 plt.savefig("co2_temp_1.png") 
+
